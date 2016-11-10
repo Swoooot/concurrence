b194d36c2e8cff22ddfdf5bb26daeeaf27c1b75c
Swoooot <baisgx@gmail.com>
2016-11-10 19:22:22 +0100

    Le script compile peut maintenant s'executer a partir d'un autre repertoire

--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-g++ -g -ansi -std=c++11 -Wall -o Person.o -c src/Person.cpp
-g++ -g -ansi -std=c++11 -Wall -o GridElement.o -c src/GridElement.cpp
-g++ -g -ansi -std=c++11 -Wall -o Grid.o -c src/Grid.cpp
-g++ -g -ansi -std=c++11 -Wall -o main.o -c src/main.cpp
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+g++ -g -ansi -std=c++11 -Wall -o Person.o -c $DIR/src/Person.cpp
+g++ -g -ansi -std=c++11 -Wall -o GridElement.o -c $DIR/src/GridElement.cpp
+g++ -g -ansi -std=c++11 -Wall -o Grid.o -c $DIR/src/Grid.cpp
+g++ -g -ansi -std=c++11 -Wall -o main.o -c $DIR/src/main.cpp
 mkdir bin
 g++ -o bin/main.exe main.o Grid.o GridElement.o Person.o -lpthread -lm 
 echo "Compilation terminée (s'il y a une erreur de mkdir, la compilation a tout de même réussi)"
