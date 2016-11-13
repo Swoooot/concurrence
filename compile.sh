f6f7a728d80e695f84844b0866b9fa648fb4c4fe
Cyprien Levy <cyprien007@gmail.com>
2016-11-13 16:04:17 +0100

    Modification script compile.sh

--- compile.sh
+++ compile.sh
@@ -5,6 +5,6 @@ g++ -g -ansi -std=c++11 -Wall -o $DIR/GridElement.o -c $DIR/src/GridElement.cpp
 g++ -g -ansi -std=c++11 -Wall -o $DIR/Grid.o -c $DIR/src/Grid.cpp
 g++ -g -ansi -std=c++11 -Wall -o $DIR/ThreadArg.o -c $DIR/src/ThreadArg.cpp
 g++ -g -ansi -std=c++11 -Wall -o $DIR/main.o -c $DIR/src/main.cpp
-mkdir bin
+mkdir -p $DIR/bin
 g++ -o $DIR/bin/main.exe $DIR/main.o $DIR/Grid.o $DIR/GridElement.o $DIR/Person.o $DIR/ThreadArg.o -lpthread -lm 
-echo "Compilation terminée (s'il y a une erreur de mkdir, la compilation a tout de même réussi)"
+echo "Compilation terminée "
